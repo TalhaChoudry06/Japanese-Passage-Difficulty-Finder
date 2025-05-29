6bd871541d384177c3f6255515f9542cef719ea3
Talha Choudry <talhachoudry16@gmail.com>
2025-05-28 18:34:40 -0700

    init client screen

---
+++ clientScreen.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+master = Tk()
+master.title("Japanese Sentence Morphology")
+master.geometry("500x300")
+
+text_box = Text(master, height=5, width=40)
+text_box.pack()
+
+text_box.focus_set()
+
+def retrieve_input():
+    inputValue= text_box.get("1.0","end-1c")
+    print(inputValue)
+
+b = Button(master, text = "OK", width = 10, command = retrieve_input)
+b.pack()
+
+mainloop()
